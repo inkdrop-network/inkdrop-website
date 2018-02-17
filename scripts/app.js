9ae0eea7f11983f29c79f5ba2fe8cf63d3c8199c
mikas <mi.kaserer@gmail.com>
2018-02-17 14:38:17 +0100

    neue anpassungen

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("a[href*=\\#]:not([href=\\#]):not('.carousel-control-prev'):not('.carousel-control-next')").on('click', function (e) {
+    $("a[href*=\\#]:not([href=\\#]):not('.carousel-control-prev'):not('.carousel-control-next'):not('.card-title')").on('click', function (e) {
         var href = $(this).attr('href');
         $('html, body').animate({
             scrollTop: $(href).offset().top
